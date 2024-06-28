f1256553fe179841125368214e25b8e9902fe2d3
Ruslan Ibragimov <ruslan.ibragimov@midge-medical.com>
2024-06-28 23:16:29 +0200

    feat: Create level model

--- src/glucosemonitor/levels/apps.py
+++ src/glucosemonitor/levels/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LevelsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'levels'
+    name = 'glucosemonitor.levels'
--- src/glucosemonitor/levels/models.py
+++ src/glucosemonitor/levels/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Level(models.Model):
+
+    class RecordingType(models.TextChoices):
+        HISTORY = "history"
+        SCAN = "scan"
+
+        @classmethod
+        def get_type_by_int_value(cls, value: int):
+            return {
+                0: cls.HISTORY,
+                1: cls.SCAN,
+            }.get(value)
+
+    user_id = models.UUIDField(db_index=True)
+    device_name = models.CharField(max_length=100)
+    device_serial_number = models.UUIDField(db_index=True)
+    device_timestamp = models.DateTimeField(db_index=True)
+
+    recording_type = models.CharField(choices=RecordingType.choices, db_index=True, max_length=15)
+    glucose_history_mg_dl = models.IntegerField(null=True, blank=True)
+    glucose_scan_mg_dl = models.IntegerField(null=True, blank=True)
+
+    fast_acting_insulin = models.CharField(max_length=100, null=True, blank=True)
+    fast_acting_insulin_units = models.FloatField(null=True, blank=True)
+
+    food_data = models.CharField(max_length=100, null=True, blank=True)
+
+    carbohydrates_grams = models.FloatField(null=True, blank=True)
+    carbohydrates_servings = models.FloatField(null=True, blank=True)
+
+    long_acting_insulin = models.CharField(max_length=100, null=True, blank=True)
+    long_acting_insulin_units = models.FloatField(null=True, blank=True)
+
+    notes = models.TextField(null=True, blank=True)
+
+    glucose_test_strip_mg_dl = models.IntegerField(null=True, blank=True)
+    ketone_mmol_l = models.FloatField(null=True, blank=True)
+
+    meal_insulin_units = models.FloatField(null=True, blank=True)
+    correction_insulin_units = models.FloatField(null=True, blank=True)
+    user_adjusted_insulin_units = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user_id}::{self.device_timestamp}::{self.recording_type}"
