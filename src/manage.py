bd6b3da37ebd7d6e284a951ce9b7d933c0d1997a
Ruslan Ibragimov <ruslan.ibragimov@midge-medical.com>
2024-06-28 23:17:15 +0200

    fix: Correct PYTHONPATH setup for src directory

--- src/manage.py
+++ src/manage.py
@@ -4,6 +4,9 @@ import os
 import sys
 
 
+sys.path.append(os.path.dirname(__file__))
+
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'glucosemonitor.settings')
