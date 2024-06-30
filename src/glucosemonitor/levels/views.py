edf4b8e129bb6b9a76751b153a335ae674701816
Ruslan Ibragimov <ruslan.ibragimov@midge-medical.com>
2024-06-30 02:04:07 +0200

    feat: Implement Ordering by glucometer values and device timestamps

--- src/glucosemonitor/levels/views.py
+++ src/glucosemonitor/levels/views.py
@@ -1,5 +1,6 @@
-from django_filters import rest_framework as filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, mixins, permissions
+from rest_framework.filters import OrderingFilter
 
 from glucosemonitor.levels.filters import LevelFilter
 from glucosemonitor.levels.models import Level
@@ -10,8 +11,9 @@ class LevelListView(generics.GenericAPIView, mixins.ListModelMixin):
     permission_classes = (permissions.IsAuthenticated,)
     queryset = Level.objects.all()
     serializer_class = LevelSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
     filterset_class = LevelFilter
+    ordering_fields = ['glucose_history_mg_dl', 'glucose_scan_mg_dl', "device_timestamp"]
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
